4bdc79e61ccc29fb20f6210d068762cdd7d0ce7d
muhammadsaefulr <saepull.id@gmail.com>
2025-09-07 14:59:05 +0700

    revamp: adding additional info about me

--- src/config.ts
+++ src/config.ts
@@ -8,16 +8,16 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "Epul Dev",
+	subtitle: "My Documentation Blogs",
+	lang: "id", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/japan-home.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -46,33 +46,38 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/muhammadsaefulr", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/109879998_p0_master1200.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/', or absolute URL
+	name: "Epul Dev",
+	bio: "Dreaming to solving problems with technology",
 	links: [
+		// {
+		// 	name: "Twitter",
+		// 	icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		// 	// You will need to install the corresponding icon set if it's not already included
+		// 	// `pnpm add @iconify-json/<icon-set-name>`
+		// 	url: "https://twitter.com",
+		// },
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			name: "Facebook",
+			icon: "fa6-brands:facebook",
+			url: "https://web.facebook.com/m.saeful.52438",
 		},
 		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			name: "Instagram",
+			icon: "fa6-brands:instagram",
+			url: "https://www.instagram.com/msaepul07_/",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/muhammadsaefulr",
 		},
 	],
 };
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,32 @@
-# About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+# About Me
 
-::github{repo="saicaca/fuwari"}
+Halo! Saya **Muhammad Saeful Rahmat**, seorang Full Stack Developer asal Bekasi, Indonesia. dan sekarang saya sedang membangun perjalanan saya di dunia teknologi dengan tujuan besar: membangun sesuatu yang menarik dan bermanfaat.
 
-> ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+---
+
+## 🔎 What I Do
+Saya menghabiskan sebagian besar waktu saya dengan:
+- Mengembangkan aplikasi berbasis **web** (frontend & backend)
+- Mendesain sistem dengan **arsitektur yang scalable**
+- Mengeksplorasi **automasi** dan integrasi sistem
+
+---
+
+## 🛠 Latest Works
+- **[Emailsopo](https://emailsopo.msaepul.my.id)** 
+Layanan validasi email dengan fitur **SMTP check** dan **bulk validation**, membantu bisnis memastikan kualitas data email agar lebih efektif digunakan untuk campaign dan menghindari fake order atau sebagainya.
+
+
+- **[Streaming App Backend Platform](https://streamapp.msaepul.my.id)**
+Backend platform untuk aplikasi streaming dengan fokus pada skalabilitas, dan integrasi dengan ranking site seperti anilist, mydramalist.
+
+
+---
+
+## 🌱 Currently Learning
+- Mencoba membuat Backend yang **high-performance** dengan Go Fiber
+- Salesing Dan Digital Marketing
+- Pemrograman sistem rendah tingkat dengan C++ & QEMU
+
+---
+_“Always Learn Something New”_
